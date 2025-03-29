f75b922b6969c42a8320dbeeea6f508743ce25ec
ammar <ammar.alnwylaty@gmail.com>
2025-03-29 23:11:23 +0000

    fixed not rotating/moving on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -224,28 +224,29 @@ const HeroCanvas = () => {
             <Cloud key={index} startPosition={position} />
           ))}
           {(!isMobile || controlsEnabled) && (
-            <OrbitControls 
-              enabled={true}
-              enableZoom={false}
-              minPolarAngle={Math.PI / 2}
-              maxPolarAngle={Math.PI / 2}
-              enableDamping={true}
-              dampingFactor={0.05}
-              rotateSpeed={isMobile ? 2 : 1.5}
-              panSpeed={1.2}
-              touches={{
-                one: 'rotate',
-                two: 'drag'
-              }}
-              mouseButtons={{
-                LEFT: THREE.MOUSE.ROTATE,
-                MIDDLE: THREE.MOUSE.DOLLY,
-                RIGHT: THREE.MOUSE.PAN
-              }}
-              domElement={document.querySelector('canvas')}
-              enableTouchRotate={controlsEnabled}
-              enableTouchPan={controlsEnabled}
-            />
+          <OrbitControls
+          makeDefault
+          enabled={controlsEnabled}
+          enableRotate={controlsEnabled}
+          enableZoom={false}
+          enablePan={controlsEnabled}
+          enableDamping={true}
+          dampingFactor={0.05}
+          rotateSpeed={isMobile ? 1.2 : 1.5}
+          panSpeed={1.2}
+          enableTouchRotate={controlsEnabled}
+          enableTouchPan={controlsEnabled}
+          touches={{
+            ONE: THREE.TOUCH.ROTATE,
+            TWO: THREE.TOUCH.PAN,
+          }}
+          mouseButtons={{
+            LEFT: THREE.MOUSE.ROTATE,
+            MIDDLE: THREE.MOUSE.DOLLY,
+            RIGHT: THREE.MOUSE.PAN,
+          }}
+        />
+            
           )}
         </Suspense>
       </Canvas>
@@ -274,25 +275,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas />
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div> */}
+   
 
       {/* Add the floating design elements */}
       <div className="absolute inset-0">
